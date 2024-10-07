4677b2f4a3ae4431be7e1ed38ca6bfa09156f786
hubnig <hubniggera@mail.ru>
2024-10-08 00:07:12 +0300

    FEAT: add user

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Patch,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { UserDto } from './user.dto';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Auth()
+	@Get('profile')
+	async getProfile(@CurrentUser('id') id: number) {
+		return this.userService.byId(id);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Put('profile')
+	async updateProfile(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+		return this.userService.updateProfile(id, dto);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Patch('profile/favorites/:productId')
+	async toggleFavorites(
+		@CurrentUser('id') id: number,
+		@Param('productId') productId: string
+	) {
+		return this.userService.toggleFavorites(id, +productId);
+	}
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,85 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { hash } from 'argon2';
+import { PrismaService } from 'src/prisma.service';
+import { returnUserObject } from './return-user.object';
+import { UserDto } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async byId(id: number, selectObject: Prisma.UserSelect = {}) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id
+			},
+			select: {
+				...returnUserObject,
+				favorites: {
+					select: {
+						id: true,
+						name: true,
+						price: true,
+						images: true,
+						slug: true
+					}
+				},
+				...selectObject
+			}
+		});
+
+		if (!user) throw new Error('User not found');
+
+		return user;
+	}
+
+	async updateProfile(id: number, dto: UserDto) {
+		const isSameUser = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		});
+
+		if (isSameUser && String(id) !== String(isSameUser.id))
+			throw new Error('Email already exists');
+
+		const user = await this.byId(id);
+
+		return this.prisma.user.update({
+			where: {
+				id
+			},
+			data: {
+				email: dto.email,
+				name: dto.name,
+				phone: dto.phone,
+				avatarPath: dto.avatarPath,
+				password: dto.password ? await hash(dto.password) : user.password
+			}
+		});
+	}
+
+	async toggleFavorites(userId: number, productId: number) {
+		const user = await this.byId(userId);
+
+		if (!user) throw new Error('User not found');
+
+		const isExist = user.favorites.some(favorite => favorite.id === productId);
+
+		await this.prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				favorites: {
+					[isExist ? 'disconnect' : 'connect']: {
+						id: productId
+					}
+				}
+			}
+		});
+
+		return 'Seccess';
+	}
+}
